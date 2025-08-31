c4641805b081cff6f2c6617e3c02ffcfd79cec34
d-kja <89911360+d-kja@users.noreply.github.com>
2025-08-30 22:55:03 -0300

    idk

--- dasher/core.cpp
+++ dasher/core.cpp
@@ -1,5 +1,4 @@
 #include "raylib.h"
-#include <cstdio>
 #include <iostream>
 
 const int window_width = 1200;
@@ -10,32 +9,48 @@ float delta = 0.0;
 const float movement_positive_offset = 1.5;
 const float movement_negative_offset = 1.0;
 
-// force = mass * acceleration;
-float acceleration = 10;
 const float gravity = 9.8; // 9.8 m/s -> 0.8 -> acceleration due to gravity;
 
 //
 // Character
 //
 
+const float amount_sprites = 9.0;
+const float animation_update_time = 1.0 / 12.0;
+
+int animation_frame = 0;
+float animation_running_time = 0.0;
+
+// force = mass * acceleration;
 int mass = 10;
+int direction = 0;
 float velocity = 0.0;
 
-const int char_height = 40;
-const int char_width = 25;
+Vector2 character_position;
+Texture2D character_texture;
+Rectangle character_rectangle;
 
-int get_floor_y() { return window_height - char_height; }
-int get_center_x() { return window_width / 2.0; }
+int get_floor_y() { return window_height - character_rectangle.height; }
+int get_center_x() {
+  return window_width / 2.0 - character_rectangle.width / 2;
+}
 
-float character_x = get_center_x();
-float character_y = get_floor_y();
+int get_char_left_bound_x() { return character_position.x; }
+int get_char_right_bound_x() {
+  return character_position.x + character_rectangle.width;
+}
+int get_char_upper_bound_y() { return character_position.y; }
+int get_char_lower_bound_y() {
+  return character_position.y + character_rectangle.height;
+}
 
-int get_char_left_bound_x() { return character_x; }
-int get_char_right_bound_x() { return character_x + char_width; }
-int get_char_upper_bound_y() { return character_y; }
-int get_char_lower_bound_y() { return character_y + char_height; }
+bool is_floor() { return character_position.y >= get_floor_y(); }
 
-bool is_floor() { return character_y >= get_floor_y(); }
+//
+// Hazard
+//
+
+Texture hazard_texture;
 
 float print_debug = 0;
 void debug_state() {
@@ -46,8 +61,8 @@ void debug_state() {
   }
 
   std::cout << "DELTA: " << delta << std::endl;
-  std::cout << "CHARACTER X: " << character_x << std::endl;
-  std::cout << "CHARACTER Y: " << character_y << std::endl;
+  std::cout << "CHARACTER X: " << character_position.x << std::endl;
+  std::cout << "CHARACTER Y: " << character_position.y << std::endl;
   std::cout << "GRAVITY: " << gravity << std::endl;
   std::cout << "VELOCITY: " << velocity << std::endl;
   std::cout << "FLOOR: " << is_floor() << std::endl;
@@ -59,7 +74,7 @@ void reset_movement() { velocity = 0.0; }
 void apply_gravity() { velocity += gravity * delta; }
 void check_boundaries() {
   if (is_floor()) {
-    character_y = get_floor_y();
+    character_position.y = get_floor_y();
     velocity = 0;
   }
 }
@@ -72,18 +87,51 @@ void controller() {
   }
 
   apply_gravity();
-  character_y += velocity;
+  character_position.y += velocity;
 
   check_boundaries();
 }
 
 void character() {
-  DrawRectangle(character_x, character_y, char_width, char_height, RED);
+  animation_running_time += delta;
+
+  if (!is_floor()) {
+    if (animation_running_time >= (animation_update_time * 2.5)) {
+      animation_running_time = 0.0;
+      animation_frame += 1;
+    }
+  } else {
+    if (animation_running_time >= animation_update_time) {
+      animation_running_time = 0.0;
+      animation_frame += 1;
+    }
+  }
+
+  if (animation_frame >= 9) {
+    animation_frame = 0;
+  }
+
+  character_rectangle.x = character_rectangle.width * animation_frame;
+
+  DrawTextureRec(character_texture, character_rectangle, character_position,
+                 WHITE);
 }
 
 int main() {
   InitWindow(window_width, window_height, window_title);
   SetTargetFPS(60);
+
+  // Load sprite
+  character_texture = LoadTexture("./assets/character.png");
+  hazard_texture = LoadTexture("./assets/hazard.png");
+
+  character_rectangle.width = character_texture.width / amount_sprites;
+  character_rectangle.height = character_texture.height;
+  character_rectangle.x = 0;
+  character_rectangle.y = 0;
+
+  character_position.x = get_center_x();
+  character_position.y = get_floor_y();
 
   while (true) {
     bool shouldCloseWindow =
@@ -91,6 +139,10 @@ int main() {
 
     if (shouldCloseWindow) {
       debug_state();
+
+      UnloadTexture(character_texture);
+      UnloadTexture(hazard_texture);
+
       CloseWindow();
       return 0;
     }
