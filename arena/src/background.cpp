70b36d9f10cdccf625b1ef138b3a8064783fd5f3
d-kja <89911360+d-kja@users.noreply.github.com>
2025-09-10 02:00:05 -0300

    ref

--- arena/src/background.cpp
+++ arena/src/background.cpp
@@ -1,4 +1,6 @@
 #include "./background.h"
+#include <array>
+#include <iostream>
 
 float BASE_SCALE = 4.0;
 
@@ -17,8 +19,8 @@ std::array<float, 2> Background::get_background_center_offset() {
   std::array<float, 2> axis = this->get_background_size();
 
   // WINDOW OFFSET WITH CURRENT TEXTURE
-  float offset_x = (width - axis[0]);
-  float offset_y = (height - axis[1]);
+  float offset_x = (width - axis[0] / 2.0);
+  float offset_y = (height - axis[1] / 2.0);
 
   std::array<float, 2> offsets{offset_x, offset_y};
 
@@ -28,6 +30,7 @@ std::array<float, 2> Background::get_background_center_offset() {
 void Background::clean_up_background() { UnloadTexture(this->texture); }
 void Background::setup_background(const char *path) {
   this->texture = LoadTexture(path);
+
   std::array<float, 2> center = this->get_background_center_offset();
 
   this->position.x = center[0];
@@ -37,20 +40,30 @@ void Background::setup_background(const char *path) {
 }
 
 void Background::render_background(float char_width, float char_height) {
-  float width = GetRenderWidth() / 2.0;
-  float height = GetRenderHeight() / 2.0;
-  float text_width = this->texture.width;
-  float text_height = this->texture.height;
+  std::array<float, 2> background_size = this->get_background_size();
+  std::array<float, 2> background_offset = this->get_background_center_offset();
 
-  std::cout << "WIDTH -> " << width << std::endl;
-  std::cout << "HEIGHT -> " << height << std::endl;
+  float background_width = background_size[0];
+  float background_height = background_size[1];
 
-  std::cout << "POS X -> " << this->position.x << std::endl;
-  std::cout << "POS Y -> " << this->position.y << std::endl;
+  float background_offset_x = background_offset[0] * 2.0;
+  float background_offset_y = background_offset[1] * 2.0;
 
-  float left_bound = width - 200;
+  float left_bound = background_width / 2.0 + background_offset_x;
+  float right_bound =(background_width / 2.0 * -1.0) + background_offset_x;
+
+  std::cout << "POS X: " << this->position.x << std::endl;
+  std::cout << "POS Y: " << this->position.y << std::endl;
+  std::cout << "LEFT BOUND OFFSET X: " << background_offset_x << std::endl;
+  std::cout << "LEFT BOUND OFFSET Y: " << background_offset_y << std::endl;
+  std::cout << "LEFT BOUND X: " << left_bound << std::endl;
+  std::cout << "RIGHT BOUND X: " << right_bound << std::endl;
 
   if (left_bound < this->position.x) {
+    this->position = this->last_position;
+  }
+
+  if (right_bound > this->position.x) {
     this->position = this->last_position;
   }
 
